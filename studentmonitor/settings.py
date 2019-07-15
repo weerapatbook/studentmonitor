8aa68902b5fb70f7b2db9ea0a253bf6d85bd95f2
weerapatbook <book2555@hotmail.co.th>
2019-07-15 23:05:54 +0700

    init

--- studentmonitor/settings.py
+++ studentmonitor/settings.py
@@ -119,5 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS=(
+    os.path.join(BASE_DIR, 'static')
+)
