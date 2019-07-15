31a3cd821abc34e3a094e3cbaaa06ecb758d4565
weerapatbook <book2555@hotmail.co.th>
2019-07-15 23:19:27 +0700

    init

--- studentmonitor/settings.py
+++ studentmonitor/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm6vwx)5q(skdtc84h70#@r!q288p+9n(!c1(s4l-(zt#@=i)*&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
